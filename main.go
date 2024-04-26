79d989cdcba60ab4af3307400c61fe8c5ce9cfa1
JH1 Jsociety <jsocyety@proton.me>
2024-04-26 16:20:11 -0600

    base

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "github.com/gin-gonic/gin"
+    "net/http"
+)
+
+func main() {
+    router := gin.Default()
+
+    // Endpoint para verificar si el servidor está funcionando
+    router.GET("/alive", func(c *gin.Context) {
+        c.String(http.StatusOK, "Alive")
+    })
+
+    // Un segundo endpoint para mostrar un mensaje específico
+    router.GET("/obedece", func(c *gin.Context) {
+        c.String(http.StatusOK, "Obedece")
+    })
+
+    // Un endpoint raíz que simplemente devuelve una respuesta básica
+    router.GET("/", func(c *gin.Context) {
+        c.String(http.StatusOK, "Root Endpoint")
+    })
+
+    // Inicia el servidor en el puerto 8080
+    router.Run(":8080")
+}
