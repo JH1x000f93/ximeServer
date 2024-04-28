3499bdfaf2a942d3202f466f8aca0dc08f135fe8
JH1 Jsociety <jsocyety@proton.me>
2024-04-28 13:56:57 -0600

    add sms number

--- main.go
+++ main.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-    "net/http"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
-    router := gin.Default()
+	router := gin.Default()
 
-    // Endpoint para verificar si el servidor está funcionando
-    router.GET("/alive", func(c *gin.Context) {
-        c.String(http.StatusOK, "Alive")
-    })
+	// Endpoint para verificar si el servidor está funcionando
+	router.GET("/alive", func(c *gin.Context) {
+		c.String(http.StatusOK, "Alive")
+	})
 
-    // Un segundo endpoint para mostrar un mensaje específico
-    router.GET("/obedece", func(c *gin.Context) {
-        c.String(http.StatusOK, "sms:")
-    })
+	// Un segundo endpoint para mostrar un mensaje específico
+	router.GET("/obedece", func(c *gin.Context) {
+		c.String(http.StatusOK, "sms:+50368304843|hola")
+	})
 
-    // Un endpoint raíz que simplemente devuelve una respuesta básica
-    router.GET("/", func(c *gin.Context) {
-        c.String(http.StatusOK, "Root Endpoint")
-    })
+	// Un endpoint raíz que simplemente devuelve una respuesta básica
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Root Endpoint")
+	})
 
-    // Inicia el servidor en el puerto 8080
-    router.Run(":8080")
+	// Inicia el servidor en el puerto 8080
+	router.Run(":8080")
 }
